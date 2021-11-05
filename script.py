3e0f57d5c80d74f5085dae41e8454c229617c565
NielzosFilms <nielzosfilms@gmail.com>
2021-11-05 14:43:01 +0100

    requirements

--- script.py
+++ script.py
@@ -1,5 +1,4 @@
 from ResponseHandler import ResponseHandler
-# pip install mysql-connector-python
 from database import Database
 expression = "[-a-zA-Z0-9@: % ._\+~  # =]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"
 
