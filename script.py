6b296e571245595b12200ab76e2eea502b23155e
JoristerWolbeek <joris.terwolbeek@outlook.com>
2021-11-05 14:33:35 +0100

    Made connection to DB, creates DB on start up

--- script.py
+++ script.py
@@ -1,1 +1,5 @@
+# pip install mysql-connector-python
+from database import Database
 expression = "[-a-zA-Z0-9@: % ._\+~  # =]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"
+
+db = Database()
