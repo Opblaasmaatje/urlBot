ba37127512db762636a0fa848573a3dd78490f8b
JoristerWolbeek <joris.terwolbeek@outlook.com>
2021-11-05 14:58:39 +0100

    add entry to db

--- script.py
+++ script.py
@@ -2,4 +2,7 @@
 from database import Database
 expression = "[-a-zA-Z0-9@: % ._\+~  # =]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"
 
+link = "test"
+
 db = Database()
+# db.addEntry(link)
